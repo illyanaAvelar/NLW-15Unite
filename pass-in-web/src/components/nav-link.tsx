57c2f9055b2588cf19719fe6d767524a757ac822
illyanaAvelar <illyavelar012@gmail.com>
2024-06-27 17:24:28 -0300

    adding IconButton component and adding component props to anchor

--- pass-in-web/src/components/nav-link.tsx
+++ pass-in-web/src/components/nav-link.tsx
@@ -1,11 +1,12 @@
-interface NavLinkProps {
+import { ComponentProps } from "react";
+
+interface NavLinkProps extends ComponentProps<"a"> {
   children: string;
-  href: string;
 }
 
 export function NavLink(props: NavLinkProps) {
   return (
-    <a href={props.href} className="font-medium text-sm text-zinc-3200">
+    <a {...props} className="font-medium text-sm text-zinc-3200">
       {props.children}
     </a>
   );
