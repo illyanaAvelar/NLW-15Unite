6eab189a836b1ecbaa8dd8c7640aaeda03459d51
illyanaAvelar <illyavelar012@gmail.com>
2024-07-04 10:32:19 -0300

    adding register for event route

--- pass-in-server-node/src/server.ts
+++ pass-in-server-node/src/server.ts
@@ -5,6 +5,7 @@ import {
   validatorCompiler,
 } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
+import { registerForEvent } from "./routes/register-for-event";
 
 export const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -12,6 +13,7 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent);
+app.register(registerForEvent);
 
 app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
   console.log("HTTP server running!");
