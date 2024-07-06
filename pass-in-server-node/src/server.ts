894f6dfa92266972e22b93023f4d893ddd4a882a
illyanaAvelar <illyavelar012@gmail.com>
2024-07-06 13:49:08 -0300

    adding checkin table and route

--- pass-in-server-node/src/server.ts
+++ pass-in-server-node/src/server.ts
@@ -8,6 +8,7 @@ import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
+import { checkIn } from "./routes/checkin";
 
 export const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -18,6 +19,7 @@ app.register(createEvent);
 app.register(registerForEvent);
 app.register(getEvent);
 app.register(getAttendeeBadge);
+app.register(checkIn);
 
 app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
   console.log("HTTP server running!");
