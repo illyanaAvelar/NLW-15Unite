1d5df1c8d15708921a8f19deb45737d602fffa01
illyanaAvelar <illyavelar012@gmail.com>
2024-07-18 08:25:41 -0300

    initial setup

--- pass-in-mobile/src/app/register.tsx
+++ pass-in-mobile/src/app/register.tsx
@@ -1,11 +1,11 @@
 import { Input } from "@/components/input";
 import { Image, StatusBar, View } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { FontAwesome6, MaterialIcons } from "@expo/vector-icons";
 import { colors } from "@/styles/colors";
 import { Button } from "@/components/button";
 import { Link } from "expo-router";
 
-export default function Home() {
+export default function Register() {
   return (
     <View className="flex-1 bg-green-500 items-center justify-center p-8">
       <StatusBar barStyle="light-content" />
@@ -17,18 +17,27 @@ export default function Home() {
       />
       <View className="w-full mt-12 gap-3">
         <Input>
-          <MaterialCommunityIcons
-            name="ticket-confirmation-outline"
+          <FontAwesome6
+            name="user-circle"
             color={colors.green[200]}
             size={20}
           />
-          <Input.Field placeholder="Código do ingresso" />
+          <Input.Field placeholder="Nome completo" />
+        </Input>
+
+        <Input>
+          <MaterialIcons
+            name="alternate-email"
+            color={colors.green[200]}
+            size={20}
+          />
+          <Input.Field placeholder="Email" keyboardType="email-address" />
         </Input>
 
         <Button title="Acessar credencial" isLoading />
 
-        <Link href={"/register"} className="text-gray-100 text-base ">
-          Ainda não possui ingresso?
+        <Link href={"/"} className="text-gray-100 text-base ">
+          Já possui ingresso?
         </Link>
       </View>
     </View>
